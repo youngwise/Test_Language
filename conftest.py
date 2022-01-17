661376b34158eb4567636d0e96c4f31f77c61951
Sergey <69456716+youngwise@users.noreply.github.com>
2022-01-17 12:15:22 +0300

    Final code editing

--- conftest.py
+++ conftest.py
@@ -3,13 +3,20 @@ import pytest
 from selenium.webdriver.chrome.options import Options
 
 
+# обработчик, который считывает из командной строки параметр language
+# handler that reads the language parameter from the command line
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None, help='Choose language')
 
+
+# реализация логики запуска браузера с указанным языком пользователя
+# implementation of the browser launch logic with the specified user language
 @pytest.fixture(scope='function')
 def browser(request):
     language = request.config.getoption('language')
 
+    # проверка, что параметр language указан
+    # checking that the language parameter is specified
     if language is None:
         raise pytest.UsageError('--language should be don\'t None!')
 
--- test_items.py
+++ test_items.py
@@ -1,8 +1,19 @@
 from selenium.webdriver.common.by import By
+
 link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
+# Код работает на браузере Chrome
+# Code works on Chrome browser
 
 def test_guest_should_see_button_add_to_basket(browser):
+    # открытие ссылки
+    # link opening
     browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR, 'button.btn-add-to-basket')
+
+    # проверка на наличие кнопки добавления в корзину
+    # check for add to cart button
+    button = browser.find_element(By.CLASS_NAME, 'btn-add-to-basket')
+
+    # если кнопка не найдена, то возвращает ошибку
+    # if the button is not found, it returns an error
     assert button is not None, 'element "button" is not found!'
